3f1d06c890007d046ef75bda214e2170b6ed94c9
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-07-13 13:42:05 +0300

    add parse env variables

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,8 @@ const config = {
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+  masterKey: process.env.MASTER_KEY || '', 
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
