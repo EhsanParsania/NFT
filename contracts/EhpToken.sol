abf73801a3d5d4c44d22b66169a6fef9130d4f58
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-07-14 12:41:46 +0300

    add manual mint for admin

--- contracts/EhpToken.sol
+++ contracts/EhpToken.sol
@@ -5,9 +5,17 @@ import "./ERC20.sol";
 pragma solidity ^0.8.0;
 
 contract EhpToken is ERC20 {
+    address public admin;
+
     constructor(string memory _name, string memory _symbol)
         ERC20(_name, _symbol)
     {
         _mint(msg.sender, 69000000 * 10**18);
+        admin = msg.sender;
+    }
+
+    function mint(address _to, uint256 amount) external {
+        require(msg.sender == admin);
+        _mint(_to, amount);
     }
 }
