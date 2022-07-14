cd746054c3bbb815d188b629ec8c206e1e0ae390
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-07-14 12:43:13 +0300

    fix error text to access to mint function

--- contracts/EhpToken.sol
+++ contracts/EhpToken.sol
@@ -15,7 +15,7 @@ contract EhpToken is ERC20 {
     }
 
     function mint(address _to, uint256 amount) external {
-        require(msg.sender == admin);
+        require(msg.sender == admin, "Only admin can mint !");
         _mint(_to, amount);
     }
 }
