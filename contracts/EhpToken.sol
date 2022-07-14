6e9659240677e29e484de99d667d570fd7cada35
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-07-14 12:35:24 +0300

    determine all supply and send to creator's wallet

--- contracts/EhpToken.sol
+++ contracts/EhpToken.sol
@@ -8,5 +8,6 @@ contract EhpToken is ERC20 {
     constructor(string memory _name, string memory _symbol)
         ERC20(_name, _symbol)
     {
+        _mint(msg.sender, 69000000 * 10**18);
     }
 }
