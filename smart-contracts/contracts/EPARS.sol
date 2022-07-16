b82273e5cbcf1c7926816a09a602a94bb51dfa57
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-07-16 15:22:38 +0300

    change token name

--- smart-contracts/contracts/EPARS.sol
+++ smart-contracts/contracts/EPARS.sol
@@ -7,19 +7,17 @@ pragma solidity ^0.8.0;
 contract EhpToken is ERC20 {
     address public admin;
 
-    constructor(string memory _name, string memory _symbol)
-        ERC20(_name, _symbol)
-    {
+    constructor() ERC20("epars", "EPARS") {
         _mint(msg.sender, 69000000 * 10**18);
         admin = msg.sender;
     }
 
-    function mint(address to, uint amount) external {
+    function mint(address to, uint256 amount) external {
         require(msg.sender == admin, "Only admin can mint !");
         _mint(to, amount);
     }
 
-    function burn(uint amount) external {
-      _burn(msg.sender,amount);
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
     }
 }
