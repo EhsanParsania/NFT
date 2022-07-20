ef389748d1385ddfe6c018d8d52744b93d788abe
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-07-20 21:29:38 +0300

    add transfer to the token

--- smart-contracts/contracts/EPARS.sol
+++ smart-contracts/contracts/EPARS.sol
@@ -4,20 +4,29 @@ import "./ERC20.sol";
 
 pragma solidity ^0.8.0;
 
-contract EhpToken is ERC20 {
-    address public admin;
+contract EPARS is ERC20 {
+    address public owner;
 
     constructor() ERC20("epars", "EPARS") {
         _mint(msg.sender, 69000000 * 10**18);
-        admin = msg.sender;
+        owner = msg.sender;
     }
 
     function mint(address to, uint256 amount) external {
-        require(msg.sender == admin, "Only admin can mint !");
+        require(msg.sender == owner, "Only owner can mint !");
         _mint(to, amount);
     }
 
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
+    }
+
+    function transfer(address _to, uint256 _amount)
+        public
+        override
+        returns (bool success)
+    {
+        _transfer(owner, _to, _amount);
+        return true;
     }
 }
