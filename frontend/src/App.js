52f3adc31e02efe8b20dd30a4c0a4aafe4f70459
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-07-23 12:34:26 +0300

    init server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p>sdgds
         <a
           className="App-link"
           href="https://reactjs.org"
