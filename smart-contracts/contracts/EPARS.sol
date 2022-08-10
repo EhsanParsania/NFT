de4341b22ee53dec344fc09a1168c4ea3cdc619b
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-08-10 09:34:55 +0300

    use context to make the token transfers safer

--- smart-contracts/contracts/EPARS.sol
+++ smart-contracts/contracts/EPARS.sol
@@ -8,17 +8,17 @@ contract EPARS is ERC20 {
     address public owner;
 
     constructor() ERC20("epars", "EPARS") {
-        _mint(msg.sender, 69000000 * 10**18);
-        owner = msg.sender;
+        _mint(_msgSender(), 69000000 * 10**18);
+        owner = _msgSender();
     }
 
     function mint(address to, uint256 amount) external {
-        require(msg.sender == owner, "Only owner can mint !");
+        require(_msgSender() == owner, "Only owner can mint !");
         _mint(to, amount);
     }
 
     function burn(uint256 amount) external {
-        _burn(msg.sender, amount);
+        _burn(_msgSender(), amount);
     }
 
     function transfer(address _to, uint256 _amount)
