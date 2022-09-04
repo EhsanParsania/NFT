9744b39cea0634f702185a0796c2857e05c428f6
Ehsan Parsania <ehsan.parsa69@gmail.com>
2022-09-04 21:52:26 +0300

    check for private key in .secret or env

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -3,7 +3,28 @@ require('@nomicfoundation/hardhat-toolbox')
 require('@nomiclabs/hardhat-etherscan')
 require('hardhat-watcher')
 
+let OWNER_PRIVATE_KEY
+if (process.env.OWNER_PRIVATE_KEY && process.env.OWNER_PRIVATE_KEY.length === 64) {
+  OWNER_PRIVATE_KEY = process.env.OWNER_PRIVATE_KEY
+} else {
+  try {
+    const fs = require('fs')
+    const secret = fs.readFileSync('.secret', 'utf8').trim()
+    if (secret.length === 64) {
+      OWNER_PRIVATE_KEY = secret
+    }
+  } catch (err) {
+    // ignore if local
+    const yargs = require('yargs/yargs')
+    const argv = yargs(process.argv).argv
+    if (argv.network && argv.network !== 'hardhat' && argv.network !== 'localhost') {
+      console.error('ERROR: when using a non-local network, an owner wallet secret should be provided to run scripts as a hex value, either in a .secret file or in the OWNER_PRIVATE_KEY env variable.')
+      process.exit(1)
+    }
+  }
+}
 
+const accounts = OWNER_PRIVATE_KEY ? [OWNER_PRIVATE_KEY] : undefined
 
 module.exports = {
   solidity: '0.8.9',
