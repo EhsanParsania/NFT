1d94da5c8612d33b1804e92c40e314081359c4ce
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-12-01 12:10:27 +0300

    init nft and constructor

--- smart-contracts/contracts/NFT.sol
+++ smart-contracts/contracts/NFT.sol
@@ -2,4 +2,12 @@
 
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721, Ownable {
+
+    constructor() ERC721("Parse", "PARSE") {}
+
+}
