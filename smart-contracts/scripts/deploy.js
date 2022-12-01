b80e8b82eefcf3eb418de9f30364a1be8ab84548
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-12-01 12:32:54 +0300

    clean deploy code

--- smart-contracts/scripts/deploy.js
+++ smart-contracts/scripts/deploy.js
@@ -1,11 +1,4 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require('hardhat')
-
 const fs = require('fs')
 const path = require('path')
 const buildPath = path.resolve(__dirname, '../build')
@@ -29,7 +22,7 @@ async function main() {
 
   fs.writeFileSync(path.join(buildPath, 'deployed contract addresses'),
     `EPARS = ${epars.address}
-  EparsFactory = ${eparsFactory.address}`
+     EparsFactory = ${eparsFactory.address}`
   )
 }
 
