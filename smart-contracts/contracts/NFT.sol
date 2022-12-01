8394159c3e42ea723f7476675681d416a977fe88
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-12-01 12:08:34 +0300

    import counter lib

--- smart-contracts/contracts/NFT.sol
+++ smart-contracts/contracts/NFT.sol
@@ -2,4 +2,4 @@
 
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";import "@openzeppelin/contracts/access/Ownable.sol";
