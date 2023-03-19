38e3707d1892c8c676f031afbe25a0e848b908bb
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2023-03-19 19:35:03 +0300

    fix app routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './globalStyles.css'
+import { AppRoutes } from './routes/Routes';
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>sdgds
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
