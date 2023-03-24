38d617b5fbe9b3c50f72fd05bf32619883ec9d38
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2023-03-24 20:36:10 +0300

    add contract verification function

--- smart-contracts/scripts/deploy.js
+++ smart-contracts/scripts/deploy.js
@@ -26,7 +26,31 @@ async function main() {
 
   fs.writeFileSync(path.join(buildPath, 'deployed contract addresses'),
     `EPARS = ${epars.address}
+     NFT = ${nft.address}`
   )
+
+  verifyContract(nft, 'NFT')
+}
+
+
+async function verifyContract(token, contractName) {
+  if (!isPublicNetwork) {
+    // verifying contracts are only needed for public network deployments
+    return
+  }
+  try {
+    await hre.run('verify:verify', { address: token.address })
+    console.log(`\n\n --> ${contractName} verified on the scanner site`)
+  } catch (err) {
+    const already = err.message?.toLowerCase()?.includes('already verified')
+    if (already) {
+      console.log(`     Verification error: ${err.message}`)
+    } else {
+      // possible error reasons: API key is not set, network connection errors (we should be able to try again, to verify only)
+      console.log(`\n\n --> ${contractName} is not verified on the scanner site\n`)
+      throw err
+    }
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
