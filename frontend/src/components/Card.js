791339e440189ac6e267b2431ee3125050bb5b36
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2023-03-24 20:36:31 +0300

    edit component name

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,11 +1,10 @@
 import { CryptoCards, Button } from '@web3uikit/core';
 
-export const NFTCard = () => (
+export const Card = () => (
     <>
         <CryptoCards
             chain='binance'
             bgColor='#000'
-            textColor="yellow"
             chainType="Network"
             onClick={console.log}
         />
--- frontend/src/components/NFTCard/NFTCard.js
+++ frontend/src/components/NFTCard/NFTCard.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './NFTCard.css';
 
 const NFTCard = ({ nft }) => {
-    const { imageUrl, title , address , owner , price  } = nft;
+    const { imageUrl, title, address, owner, price } = nft;
 
     return (
         <div className="nft-card">
             <img className="nft-image" src={imageUrl} alt={title} />
             <div className="nft-content">
                 <h3 className="nft-title">{title}</h3>
-                <hr/>
+                <hr />
                 <p className="nft-address">Address: {address}</p>
                 <div className="nft-metadata">
                     <p className="nft-owner">Owner: {owner}</p>
                 </div>
-                    <span className="nft-price">Price: {price} ETH</span> <button className='buy-nft'>Buy Now</button>
+                <span className="nft-price">Price: {price} ETH</span> <button className='buy-nft'>Buy Now</button>
             </div>
         </div>
     );
