ef847bc0d2d7319013f9203456ea953108f8c059
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2023-04-02 17:50:00 +0300

    improve nft card style and showing real data

--- frontend/src/components/NFTCard/NFTCard.js
+++ frontend/src/components/NFTCard/NFTCard.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './NFTCard.css';
+import { Hash } from '../Hash';
 
-const NFTCard = ({ nft }) => {
+const NFTCard = ({ nft, number }) => {
     const { image_data, name, owner, price = 2 } = nft;
+    const randomPrice = (Math.random() * 2).toFixed(2);
     return (
         <div className="nft-card">
             <img className="nft-image" src={image_data} alt={name} />
             <div className="nft-content">
-                <h3 className="nft-title">{name}</h3>
+                <h3 className="nft-title">#{number}</h3>
                 <hr />
-                {/* <p className="nft-address">Address: {address}</p> */}
                 <div className="nft-metadata">
-                    <p className="nft-owner">Owner: {owner}</p>
+                    <span className="nft-owner">Owner</span> <Hash hash={owner} shorten={5} clickable={true} mode='address' className={'hash-component'}/>
                 </div>
-                <span className="nft-price">Price: {price} ETH</span> <button className='buy-nft'>Buy Now</button>
+                <div className="nft-metadata">
+                    <span className="nft-price">Price </span> <span>{randomPrice} ETH</span>
+                </div>
+                <button className='buy-nft'>Buy Now</button>
             </div>
         </div>
     );
