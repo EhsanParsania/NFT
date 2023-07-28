76c21392d6bdf26c14ad5c46e999be4355c83477
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2023-07-28 12:21:40 +0300

    define dev env reader

--- server/cloud/scripts/migration/migration.js
+++ server/cloud/scripts/migration/migration.js
@@ -2,3 +2,4 @@ var db
 const _ = require('lodash')
 const { newObjectId } = require('parse-server/lib/cryptoUtils')
 const MongoClient = require('mongodb').MongoClient
+ready.dev = cb => ready(cb, 'development')
