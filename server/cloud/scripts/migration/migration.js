8e3379bd90c359d784a253d0b906c934dadcb85e
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2023-07-28 12:22:43 +0300

    add schema example data

--- server/cloud/scripts/migration/migration.js
+++ server/cloud/scripts/migration/migration.js
@@ -3,3 +3,28 @@ const _ = require('lodash')
 const { newObjectId } = require('parse-server/lib/cryptoUtils')
 const MongoClient = require('mongodb').MongoClient
 ready.dev = cb => ready(cb, 'development')
+
+/*
+// FULL INPUT EXAMPLE (boilerplate):
+upsertSCHEMA('ClassName', { // FIELDS
+  field: 'boolean|number|string|date|object|array|file|*_User|*Class',
+  requiredField: ['string', 'required'],
+  fieldWithDefault: ['string',, 'value'],
+  requiredWithDefault: ['string', 'required', 'value'],
+}, [ // INDEXES
+  {field: 1},
+  {user: 1, friend: 1, unique: true}
+], { // CLP (higher orders overrides lower)
+  //PEOPLE=all|users|admins|role:Role Name
+  //PERM=full|read|write|get|find|count|create|update|delete|addField
+  //    full=read,write
+  //    read=get,find,count
+  //    write=create,update,delete
+  'PEOPLE': 'PERM[,PERM[,...]]'
+  all: 'read',
+  users: 'write',
+  admins: 'full',
+  admins: 'full',
+  'role:Special': 'create,update',
+})
+*/
